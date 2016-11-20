0aafb472256b43b7882c3058712cee6ef1f685a1
Sergey Klimushkin <klim731995@gmail.com>
2016-11-20 17:48:17 +0400

    20.11.2016

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width">
 </head>
-<body>
+<body onresize="run()">
 <header class="header" id="headerMobile" style="display: none;">
     <a href="" class="logo__link">
         <img class="logo__img" src="img/logo.png">
@@ -19,40 +19,65 @@
 <!--<header class="header" id="headerDesktop">-->
 <!---->
 <!--</header>-->
-<div class="content">
+<div class="content clearfix">
     <div class="breadcrumbs">
         <a href="" class="breadcrumbs__item">Главная</a>
         <a href="" class="breadcrumbs__item">Работа у нас</a>
         <a href="" class="breadcrumbs__item">Вакансии</a>
         <a href="" class="breadcrumbs__item">Врач-оториноларинголог</a>
     </div>
-    <div class="vacancy">
-        <h2 class="vacancy__name">Врач-оториноларинголог</h2>
+    <h2 class="vacancy__name">Врач-оториноларинголог</h2>
+    <div class="sidemenu" id="sidemenu-desktop">
+        <h3>Адреса поликлиник</h3>
+        <a href="" class="address-list">Списком</a>
+        <div class="map"></div>
+    </div>
+    <div class="vacancy clearfix">
         <div class="vacancy__info">
-        <span class="vacancy__price">От 50 000 рублей</span>
-        <span class="vacancy__stage">Опыт работы от 1 до 3 лет</span>
-        <span class="vacancy__education">Высшее образование</span>
+            <div class="vacancy__info--first">
+                <span class="vacancy__price">От 50 000 рублей</span>
+                <span class="vacancy__stage">Опыт работы от 1 до 3 лет</span>
+                <span class="vacancy__education">Высшее образование</span>
+            </div>
             <div class="vacancy__info--second">
                 <span class="vacancy__category">Оториноларингология</span>
                 <span class="vacancy__date">7 декабря</span>
             </div>
         </div>
-        <h3 class="vacancy__duties">Должностные обязанности</h3>
-        <ul class="duties-list custom-list">
+        <form action="" method="" class="vacancy-form" id="form-desktop">
+            <span class="vacancy-form__span">Откликнуться на вакансию</span>
+            <input type="text" name="name" class="vacancy-form__name" placeholder="ФИО*" required>
+            <input type="number" name="phone" class="vacancy-form__phone" placeholder="Телефон*" required>
+            <input type="email" name="email" class="vacancy-form__email" placeholder="Email">
+            <input type="text" name="city" class="vacancy-form__city" placeholder="Город">
+            <label class="vacancy-form__file-label">
+                <span class="vacancy-form__file-button">ПРИКРЕПИТЬ ФАЙЛ</span>
+                <input type="file" name="file" class="vacancy-form__file">
+            </label>
+            <span class="vacancy-form__file-span">Загрузите файл в формате MS Word или PDF</span>
+            <textarea class="vacancy-form__message" placeholder="Сопроводительное письмо" rows="4"></textarea>
+            <label class="vacancy-form__agree-label">
+                <input type="checkbox" class="vacancy-form__agree"> Я согласен на обработку <a href="" class="vacancy-form__agree-link">персональных данных</a>
+            </label>
+            <input type="submit" class="vacancy-form__submit" value="ОТПРАВИТЬ">
+        </form>
+        <div class="vacancy__description clearfix">
+            <h3 class="vacancy__duties">Должностные обязанности</h3>
+            <ul class="duties-list custom-list">
             <li class="duties-list__item custom-list__item">Амбулаторный прием пациентов (первичный, повторный)</li>
             <li class="duties-list__item custom-list__item">Квалифицированное оказание медицинской помощи в рамках амбулаторно-поликлинической практики согласно медицинским стандартам</li>
             <li class="duties-list__item custom-list__item">Ведение электронных медицинских карт</li>
         </ul>
-        <h3 class="vacancy__requirements">Требования</h3>
-        <ul class="requirements-list custom-list">
+            <h3 class="vacancy__requirements">Требования</h3>
+            <ul class="requirements-list custom-list">
             <li class="requirements-list__item custom-list__item">Образование – высшее профессиональное медицинское. Обязательно наличие действующего сертификата на осуществление медицинской деятельности в области оториноларингологии.</li>
             <li class="requirements-list__item custom-list__item">Официально подтвержденный опыт работы в должности оториноларинголога (зафиксированный в трудовой книжке).</li>
             <li class="requirements-list__item custom-list__item">Приветствуется опыт ведения амбулаторных приемов в частных медицинских учреждениях.</li>
             <li class="requirements-list__item custom-list__item">Знание ПК в объеме пользователя.</li>
             <li class="requirements-list__item custom-list__item">Личные качества: ответственность, дисциплинированность, бесконфликтность, вежливость и корректное общение с пациентами, склонность к самообразованию и профессиональному развитию.</li>
         </ul>
-        <h3 class="vacancy__offers">Мы предлагаем</h3>
-        <ul class="offers-list custom-list">
+            <h3 class="vacancy__offers">Мы предлагаем</h3>
+            <ul class="offers-list custom-list">
             <li class="offers-list__item custom-list__item">Фиксированный оклад (50 000р.), надбавки за ученую степень, стаж работы в Компании и т.п., премии (% от оказанных услуг).</li>
             <li class="offers-list__item custom-list__item">Сменный график работы.</li>
             <li class="offers-list__item custom-list__item">Оплата отпусков и больничных согласно ТК РФ.</li>
@@ -65,28 +90,32 @@
             <li class="offers-list__item custom-list__item">Оплата ресертификационных циклов.</li>
             <li class="offers-list__item custom-list__item">Сменный график работы.</li>
         </ul>
-        <form action="" method="" class="vacancy-form">
-        <span class="vacancy-form__span">Откликнуться на вакансию</span>
-        <input type="text" name="name" class="vacancy-form__name" placeholder="ФИО*" required>
-        <input type="number" name="phone" class="vacancy-form__phone" placeholder="Телефон*" required>
-        <input type="email" name="email" class="vacancy-form__email" placeholder="Email">
-        <input type="text" name="city" class="vacancy-form__city" placeholder="Город">
-        <label class="vacancy-form__file-label">
-            <span class="vacancy-form__file-button">ПРИКРЕПИТЬ ФАЙЛ</span>
-            <input type="file" name="file" class="vacancy-form__file">
-        </label>
-        <span class="vacancy-form__file-span">Загрузите файл в формате MS Word или PDF</span>
-        <textarea class="vacancy-form__message" placeholder="Сопроводительное письмо" rows="4"></textarea>
-        <label class="vacancy-form__agree-label">
-            <input type="checkbox" class="vacancy-form__agree"> Я согласен на обработку <a href="" class="vacancy-form__agree-link">персональных данных</a>
-        </label>
-        <input type="submit" class="vacancy-form__submit" value="ОТПРАВИТЬ">
-    </form>
+            </div>
+        <form action="" method="" class="vacancy-form" id="form-mobile">
+            <span class="vacancy-form__span">Откликнуться на вакансию</span>
+            <input type="text" name="name" class="vacancy-form__name" placeholder="ФИО*" required>
+            <input type="number" name="phone" class="vacancy-form__phone" placeholder="Телефон*" required>
+            <input type="email" name="email" class="vacancy-form__email" placeholder="Email">
+            <input type="text" name="city" class="vacancy-form__city" placeholder="Город">
+            <label class="vacancy-form__file-label">
+                <span class="vacancy-form__file-button">ПРИКРЕПИТЬ ФАЙЛ</span>
+                <input type="file" name="file" class="vacancy-form__file">
+            </label>
+            <span class="vacancy-form__file-span">Загрузите файл в формате MS Word или PDF</span>
+            <textarea class="vacancy-form__message" placeholder="Сопроводительное письмо" rows="4"></textarea>
+            <label class="vacancy-form__agree-label">
+                <input type="checkbox" class="vacancy-form__agree"> Я согласен на обработку <a href="" class="vacancy-form__agree-link">персональных данных</a>
+            </label>
+            <input type="submit" class="vacancy-form__submit" value="ОТПРАВИТЬ">
+        </form>
         <a href="./" class="vacancy__back">Вернуться к списку вакансий</a>
     </div>
-    <h3>Адреса поликлиник</h3>
-    <a href="" class="address-list">Списком</a>
-    <div class="map"></div>
+    <div class="sidemenu" id="sidemenu-mobile">
+        <h3>Адреса поликлиник</h3>
+        <a href="" class="address-list">Списком</a>
+        <div class="map"></div>
+    </div>
+
 </div>
 <footer>
     <ul class="footer-navigation">
@@ -116,5 +145,6 @@
     <span class="footer__copyright-lastrow footer__copyright-lastrow--heart">Сделано с</span>
     <span class="footer__copyright-lastrow">в ITECH.group</span>
 </footer>
+<script src="script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-function mobileHeader () {
-    var headerDesk = document.getElementById("headerDesktop");
-    headerDesk.cssRules("display: none;")
+function mobileForm() {
+    var formDesk = document.querySelector("#form-desktop");
+    var formMob = document.querySelector("#form-mobile");
+    if (document.body.clientWidth < 641) {
+        if (formDesk.className == "vacancy-form") {
+            formDesk.classList.add("hidden");
+        }
+        formMob.classList.remove("hidden");
+    } else {
+        formDesk.classList.remove("hidden");
+        if (formMob.className == "vacancy-form") {
+            formMob.classList.add("hidden");
+        }
+    }
+}
+
+function mobileSidemenu() {
+    var sideDesk = document.querySelector("#sidemenu-desktop");
+    var sideMob = document.querySelector("#sidemenu-mobile");
+    if (document.body.clientWidth < 641) {
+        sideDesk.classList.add("hidden")
+        sideMob.classList.remove("hidden");
+    } else {
+        sideDesk.classList.remove("hidden");
+        if (sideMob.className == "sidemenu") {
+            sideMob.classList.add("hidden");
+        }
+    }
+}
+
+function run() {
+    mobileForm();
+    mobileSidemenu();
 }
